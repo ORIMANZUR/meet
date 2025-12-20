b6c050d3b52c1c3dea920b335fd32246e19cf4e3
ORIMANZUR <oridapc@gmail.com>
2025-12-20 19:54:17 +0200

    Fix Map Key and Disable Swizzling

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyBOlMoDhdDE92tKbIFvCfC-yzPlXrDOoKI")
+    GMSServices.provideAPIKey("AIzaSyDg-cWP3zuZCRSZC-YGfiDK389c046b7K4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
